7aa99f3634d9ac1570ca0e31bcdc4a2f6c698ebf
asharma414 <abhi.sharm.414@gmail.com>
2020-04-29 18:08:07 -0400

    Done.

---
+++ lib/student.rb
@@ -0,0 +1,16 @@
+class Student
+
+    attr_reader :name
+
+    def initialize
+    end
+
+    def raise_hand
+        puts "Pick me!"
+    end
+
+    def hello
+        puts "Hey there! I'm so excited to learn stuff."
+    end
+
+end
